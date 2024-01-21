f7641bcf49714acba74047e03d3a23f9f7cfa50c
Sapan <sapan.vohra@gmail.com>
2024-01-21 12:34:32 -0500

    final assignment

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -1,6 +1,8 @@
 import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
+// import Currency from './currency'; 
+
 const AllocationForm = (props) => {
     const { dispatch,remaining  } = useContext(AppContext);
 
@@ -58,12 +60,12 @@ const AllocationForm = (props) => {
                         <option defaultValue value="Add" name="Add">Add</option>
                 <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
-
+                    <span style={{ "position":"relative","right":"-20px"}}> € </span>   
                     <input
                         required='required'
                         type='number'
                         id='cost'
-                        value={cost}
+                        value={`${cost}`}
                         style={{ marginLeft: '2rem' , size: 10}}
                         onChange={(event) => setCost(event.target.value)}>
                         </input>
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react';
 import { TiDelete } from 'react-icons/ti';
 import { AppContext } from '../context/AppContext';
+import { FaPlusCircle } from "react-icons/fa";
+import { FaMinusCircle } from 'react-icons/fa';
 
 const ExpenseItem = (props) => {
     const { dispatch } = useContext(AppContext);
@@ -25,11 +27,25 @@ const ExpenseItem = (props) => {
 
     }
 
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+
+        dispatch({
+            type: 'RED_EXPENSE',
+            payload: expense
+        });
+
+    }
+
     return (
         <tr>
         <td>{props.name}</td>
         <td>£{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
+        <td><FaPlusCircle color='green' size='2.2em' onClick={event=> increaseAllocation(props.name)}> <FaPlusCircle /> </FaPlusCircle></td>
+        <td><FaMinusCircle color='red' size='2.2em' onClick={event=> decreaseAllocation(props.name)}> <FaMinusCircle /> </FaMinusCircle></td>
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
